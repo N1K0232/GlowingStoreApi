c16c33b4f4cfd2a12b8418d79d40911d29c130eb
N1K0232 <ns.nicolasilvestri@libero.it>
2024-03-30 14:55:05 +0100

    Add controller versioning

--- src/GlowingStoreApi/Controllers/Common/ControllerBase.cs
+++ src/GlowingStoreApi/Controllers/Common/ControllerBase.cs
@@ -1,10 +1,12 @@
 using System.Net.Mime;
+using Asp.Versioning;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GlowingStoreApi.Controllers;
 
 [ApiController]
-[Route("api/[controller]")]
+[ApiVersion("1.0")]
+[Route("api/v{version:apiVersion}/[controller]")]
 [Produces(MediaTypeNames.Application.Json)]
 public abstract class ControllerBase : Microsoft.AspNetCore.Mvc.ControllerBase
 {
